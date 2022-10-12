4a980ca71332f92f35451a41f9ca8e9bc34bab75
Tushar7567 <swain.tushar00@gmail.com>
2022-10-12 21:24:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import PureComp from "./component/PureComp";
+import Hoc from "./component/Hoc";
+import {Component} from 'react'
 
-function App() {
+class App extends Component {
+  state={
+    count:0
+  }
+
+  
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <p>
+     Pure Components in React are the components which do not re-renders when the value of state and props has been updated with the same values.
+     Features of React Pure Components
+
+    --Prevents re-rendering of Component if props or state is the same
+    --Takes care of “shouldComponentUpdate” implicitly
+    --State and Props are Shallow Compared(shallowCompare performs a shallow equality check on the current props and nextProps objects as well as the current state and nextState objects.)
+
+     </p>
+     <PureComp count={this.state.count} />
+     <button onClick={()=>{this.setState({count:this.state.count+1});console.log('I am pure component')}}>Component with diff props/state</button>
+      <button onClick={()=>{this.setState({count:this.state.count});console.log('I am normal component')}}>Component with same props/state</button>
+
+      <h2>Higher order components</h2>
     </div>
   );
+  }
 }
-
+App = Hoc(App)
 export default App;
+
+// count={count} setCount={setCount}
